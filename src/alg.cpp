1c21e738596fc462b44a886dc34476e5b916cfc4
NikitaAks <99181243+NikitaAks@users.noreply.github.com>
2022-02-27 18:48:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,12 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+for (int i = 2; i < value; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -13,6 +19,22 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+int noo = 0;
+uint64_t j = value;
+while (true) {
+    j += 1;
+    for (uint64_t i = 2; i < j; ++i) {
+        if ((j % i) == 0) {
+            noo = 1;
+            break;
+        }
+    }
+    if (noo == 0) {
+        break;
+    }
+    noo = 0;
+}
+return j;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
